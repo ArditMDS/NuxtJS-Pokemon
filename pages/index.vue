a78c8dee7bdb20620b871ff61a84a74b518339d4
ArditMDS <ardit.mataj@my-digital-school.org>
2024-05-07 16:01:20 +0200

    fixed type layout

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ onMounted(async () => {
 <div class="flex justify-center flex-col">
   <h1 class="text-2xl text-center my-8">Applis pokemon de fou</h1>
   <input class="border border-black mx-10" type="text" placeholder="Chercher un pokémon par nom">
-  <div class="flex mt-8 px-10">
+  <div class="grid grid-cols-12 mt-8 px-10">
     <TypeCard v-for="type in types" :name="type.name" :image="type.image"></TypeCard>
   </div>
   <div class="grid grid-cols-4 gap-2 px-10 flex-wrap my-8 relative">
